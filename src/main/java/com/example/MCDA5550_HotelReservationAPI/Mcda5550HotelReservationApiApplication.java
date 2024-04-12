4155254b29f202ecf968832a6e4a642a4f77a5a3
A00431008 <Sudeep.Raj.Badal@smu.ca>
2024-04-11 22:15:27 -0300

    Initializes SpringBoot Application

---
+++ src/main/java/com/example/MCDA5550_HotelReservationAPI/Mcda5550HotelReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.MCDA5550_HotelReservationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mcda5550HotelReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mcda5550HotelReservationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MCDA5550_HotelReservationAPI/Mcda5550HotelReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MCDA5550_HotelReservationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mcda5550HotelReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
