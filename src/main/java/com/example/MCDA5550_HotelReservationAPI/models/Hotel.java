08f2ad23c3a77b56b2e2eae3a7359ca40b6b46e1
A00431008 <Sudeep.Raj.Badal@smu.ca>
2024-04-11 22:24:08 -0300

    creates Hotel model

---
+++ src/main/java/com/example/MCDA5550_HotelReservationAPI/models/Hotel.java
@@ -0,0 +1,44 @@
+package com.example.MCDA5550_HotelReservationAPI.models;
+
+import jakarta.persistence.*;
+
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+    private boolean availability;
+
+    public int getID() {
+        return this.id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean getAvailability() {
+        return this.availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+}
